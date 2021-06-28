add7738d3c0a1718b6e9e769b26aeb0790facb3f
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-06-28 15:17:02 +0900

    [Day28] 자바스크립트 나머지 객체 종류

---
+++ js/scripts/common.js
@@ -0,0 +1,37 @@
+function getAgeGroup(userList, keyAge) {
+    let userAgeGroup = {};
+
+    for (var user of userList) {
+        // console.log(user.age);
+
+        var ageGroup = user[keyAge].toString().charAt(0) + "0"; // 20
+        // console.log(userAgeGroup[ageGroup]);
+
+        if (userAgeGroup[ageGroup]) {
+            userAgeGroup[ageGroup]++;
+        } else {
+            userAgeGroup[ageGroup] = 1;
+        }
+    }
+
+    return userAgeGroup;
+}
+
+function getAgeGroup2(userList, keyAge) {
+    let myMap = new Map();
+
+    for (let user of userList) {
+        var ageFirtStr = user[keyAge].toString()[0];
+        var ageGroup = ageFirtStr.padEnd(2, "0");
+
+        // 2가 올텐데.. 
+        if (myMap.has(ageGroup)) {
+            myMap.set(ageGroup, myMap.get(ageGroup) + 1);
+        } else {
+            myMap.set(ageGroup, 1);
+        }
+
+    }
+
+    return myMap;
+}
