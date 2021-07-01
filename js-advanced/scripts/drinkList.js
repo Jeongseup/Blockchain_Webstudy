7b7ae222dea2b439377db02259c51876379c8418
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-07-01 09:07:31 +0900

    [UPLOAD] 강사님 코드 업로드

---
+++ js-advanced/scripts/drinkList.js
@@ -0,0 +1,54 @@
+var drinkList = [{
+
+        drinkId: "1",
+        drinkName: "코카콜라",
+        price: 700,
+        stock: 5
+    },
+    {
+
+        drinkId: "2",
+        drinkName: "오렌지주스",
+        price: 1200,
+        stock: 5
+    },
+    {
+
+        drinkId: "3",
+        drinkName: "커피",
+        price: 500,
+        stock: 5
+    },
+
+    {
+
+        drinkId: "4",
+        drinkName: "물",
+        price: 700,
+        stock: 5
+    },
+
+    {
+
+        drinkId: "5",
+        drinkName: "보리차",
+        price: 1200,
+        stock: 5
+    },
+
+    {
+
+        drinkId: "6",
+        drinkName: "포카리",
+        price: 1000,
+        stock: 5
+    },
+
+    {
+
+        drinkId: "7",
+        drinkName: "뽀로로",
+        price: 1300,
+        stock: 5
+    }
+];
